cef479b0cbfc7a07bcf713c33d73f8c93ad7e6fb
JL6666JL <1368301748@qq.com>
2026-03-22 09:51:56 +0800

    删除了没用的文件

--- jl.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES="0" accelerate launch train.py  \
---output_dir experiments/test  \
---enable_xformers_memory_efficient_attention \
---train_batch_size=4 \
---gradient_accumulation_steps=2 
--- train.sh
+++ train.sh
@@ -1,5 +1,11 @@
-CUDA_VISIBLE_DEVICES="0,1,2,3" accelerate launch train.py  \
+# CUDA_VISIBLE_DEVICES="0,1,2,3" accelerate launch train.py  \
+# --output_dir /data2/jianglei/HoliSDiP/experiments/check  \
+# --enable_xformers_memory_efficient_attention \
+# --train_batch_size=4 \
+# --gradient_accumulation_steps=2  
+
+CUDA_VISIBLE_DEVICES="0" accelerate launch train.py  \
 --output_dir /data2/jianglei/HoliSDiP/experiments/check  \
 --enable_xformers_memory_efficient_attention \
---train_batch_size=4 \
+--train_batch_size=1 \
 --gradient_accumulation_steps=2  
